d08852e2497124b5c00da0dc98449b9708ddc711
Sushil AC <sushil.ac@innovationnxt.com>
2026-08-03 10:11:43 +0545

    biometric wrapped

--- src/modules/device.module.ts
+++ src/modules/device.module.ts
@@ -66,7 +66,7 @@ export function createDeviceModule(rpc: RpcClient): DeviceSdkModule {
       ),
 
     biometric: (options?: DeviceBiometricOptions) =>
-      rpc.request<DeviceBiometricResult>(
+      rpc.request<DevicePermissionBaseResponse<DeviceBiometricResult>>(
         NAMESPACES.DEVICE,
         ACTIONS.DEVICE.BIOMETRIC,
         options,
